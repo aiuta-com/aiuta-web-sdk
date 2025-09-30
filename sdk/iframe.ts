20c39bb54fdd214d807277579bf8ad4f8f188e6d
Sergei Nevesenko <sergei.nevesenko@aiuta.com>
2025-09-30 20:21:57 +0300

    Allow web-share

--- sdk/iframe.ts
+++ sdk/iframe.ts
@@ -42,6 +42,7 @@ export default class IframeManager {
     const iframe = document.createElement('iframe') as HTMLIFrameElement
     iframe.id = this.iframeId
     iframe.src = src
+    iframe.allow = 'web-share'
 
     iframe.style.position = 'fixed'
     iframe.style.top = '0'
