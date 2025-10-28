ff32a4f2b1e62655c3bba1fe0a4a0d58f1c49a69
Sergei Nevesenko <sergei.nevesenko@aiuta.com>
2025-10-28 17:19:42 +0200

    Disable swipes on results

--- app/src/pages/5-Results/ResultsDesktop.tsx
+++ app/src/pages/5-Results/ResultsDesktop.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ResultActions, Flex, RemoteImage, Feedback } from '@/components'
-import { useResultsGallery, useSwipeGesture } from '@/hooks'
+import { useResultsGallery } from '@/hooks'
 import { combineClassNames } from '@/utils'
 import styles from './Results.module.scss'
 
@@ -8,25 +8,12 @@ import styles from './Results.module.scss'
  * Desktop version of results page with synchronized scrolling
  */
 export default function ResultsDesktop() {
-  const { slideItemIndex, images, currentImage, handleSliderItemClick } = useResultsGallery()
-
-  const swipeHandlers = useSwipeGesture(({ direction }) => {
-    if (direction === 'up' && slideItemIndex < images.length - 1) {
-      handleSliderItemClick(slideItemIndex + 1)
-    } else if (direction === 'down' && slideItemIndex > 0) {
-      handleSliderItemClick(slideItemIndex - 1)
-    }
-  })
+  const { slideItemIndex, images, currentImage } = useResultsGallery()
 
   return (
     <main className={styles.results}>
       <Flex contentClassName={combineClassNames('aiuta-image-l')}>
-        <RemoteImage
-          src={images[slideItemIndex]?.url || ''}
-          alt="Try-on image"
-          shape="L"
-          {...swipeHandlers}
-        />
+        <RemoteImage src={images[slideItemIndex]?.url || ''} alt="Try-on image" shape="L" />
         {currentImage && (
           <Feedback generatedImageUrl={currentImage.url} className={styles.feedback} />
         )}
--- app/src/pages/5-Results/ResultsMobile.tsx
+++ app/src/pages/5-Results/ResultsMobile.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Flex, RemoteImage, IconButton, Disclaimer, Feedback } from '@/components'
-import { useResultsGallery, useNavigatorShare, useSwipeGesture } from '@/hooks'
+import { useResultsGallery, useNavigatorShare } from '@/hooks'
 import { combineClassNames } from '@/utils'
 import { icons } from './icons'
 import styles from './Results.module.scss'
@@ -9,19 +9,8 @@ import styles from './Results.module.scss'
  * Mobile version of results page with share functionality and swipe navigation
  */
 export default function ResultsMobile() {
-  const { slideItemIndex, images, handleSliderItemClick } = useResultsGallery()
+  const { slideItemIndex, images } = useResultsGallery()
   const { shareImage, handleMobileImageClick } = useNavigatorShare()
-
-  const swipeHandlers = useSwipeGesture(({ direction }) => {
-    const isNext = direction === 'left' || direction === 'up'
-    const isPrev = direction === 'right' || direction === 'down'
-
-    if (isNext && slideItemIndex < images.length - 1) {
-      handleSliderItemClick(slideItemIndex + 1)
-    } else if (isPrev && slideItemIndex > 0) {
-      handleSliderItemClick(slideItemIndex - 1)
-    }
-  })
 
   const currentImageUrl = images[slideItemIndex]?.url || ''
 
@@ -33,7 +22,6 @@ export default function ResultsMobile() {
           alt="Generated result"
           shape="L"
           onClick={() => handleMobileImageClick(currentImageUrl)}
-          {...swipeHandlers}
         />
         <IconButton
           icon={icons.share}
